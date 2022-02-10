8d562ac880a612c366c36d8956facad426c8f16e
Max Kearney <ohaiguyhai@Gmail.com>
2022-02-10 13:09:56 +1100

    Controls menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,13 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
     public void StartButton()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void ControlsButton()
+    {
+        SceneManager.LoadScene(1);
     }
 }
